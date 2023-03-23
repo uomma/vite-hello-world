b79cabb79fb1cfaef6fcebcd1151cd6515d4b2fc
uomma <marcelloleone975@gmail.com>
2023-03-23 01:40:53 +0100

    blu

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <script >
-
+export default{
+  data(){
+    return {
+      message: 'La mia seconda app con Vite!'
+    }
+  }
+}
 </script>
 
 <template>
-  <h1> “La mia prima app con Vite!”</h1>
+  <h1> {{ message }}</h1>
 </template>
 
+<style>
+h1{
+  color: blue;
+}</style>
